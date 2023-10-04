d8c05005495cc44e2a3924526af0b0bc63c42b63
JamesonJC <jamesonpillay@gmail.com>
2023-10-03 19:39:53 -0700

    8. Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,6 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
+
+from = ARVG[0].scan(/frm:(.*?)\]/)
+to = ARVG[0].scan(/to:(.*?)\]/)
+flags = ARVG[0].scan(/flags:(.*?)\]/)
+puts [from, to, flags].join(',')
